5fdca2915b0de9b9ec6c84f519301c42de9fbfde
Batara Payment Network <hendra.location01@gmail.com>
2026-09-01 23:49:20 +0700

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+// POCMART SUPABASE CONFIG
+// Isi 2 nilai ini dari Supabase: Project Settings > API.
+window.POCMART_CONFIG = {
+  SUPABASE_URL: "https://pdtypybdfycjeehbunjd.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_0BSwFtA_5U3HUPJF-0H--Q_7Sj9bS8G",
+  ADMIN_WHATSAPP: "6285724159878"
+};
